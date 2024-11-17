1372068fb9a413e6ba400403dcc2a637a29c6e83
Sharini Rahman <sharinir@MacBook-Air.local>
2024-11-17 15:00:55 -0500

    Add Step 4

--- app/main.py
+++ app/main.py
@@ -9,15 +9,58 @@ import json
 import os
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Good Day": "Sunshine!"}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "yeh5kr"
 
-@app.get("/sum/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {"product": c * d}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+
+@app.get('/songs')
+def get_songs():
+    query = """
+    SELECT 
+        songs.title, 
+        songs.album, 
+        songs.artist, 
+        songs.year, 
+        songs.file, 
+        songs.image, 
+        genres.genre 
+    FROM songs
+    JOIN genres ON songs.genre = genres.genreid;
+    """
+    try:
+        cur.execute(query)
+        headers = [x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(headers, result)))
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
